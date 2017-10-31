742933b0e3c252ade609ba64cfe6b218d17c3fbc
Mayank Shah <mayank.24shah@gmail.com>
2017-10-31 01:34:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 function destructivelyAppendKitten(name){
   kittens.push(name)
-  return kittens 
+  return kittens
 }
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
